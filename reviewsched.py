7d082c6e27ade538f919d5873ad0a08e75595c96
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-06-17 21:27:39 -0300

    Remove some events

--- reviewsched.py
+++ reviewsched.py
@@ -19,42 +19,11 @@ calendar = GoogleCalendar('vcavalcanteab@gmail.com')
 today = date.today()
 
 # subject creation
-event = Event(summary=f"Notes & Summary - {session}",
+event = Event(summary=f"{session}",
               start=today,
               color_id='2')
 calendar.add_event(event)
-print("Creating Notes & Summary event...")
-
-# anki creation
-event = Event(summary=f"Exercises APT - {session}",
-              start=today,
-              color_id='7',
-              description="Crie cards Anki sobre suas anotações, e sobre seu resumo (com GPT).\n\n"
-                          "Crie questões práticas e teóricas com GPT. Adicione questões já vistas.\n\n"
-                          "Exemplo de questão GPT para o Anki: \n\n"
-                          "Oi, chat! Eu preciso criar flashcards no Anki sobre o conteúdo do resumo/documento"
-                          " em anexo. Por favor, extraia as informações principais e crie perguntas e respostas"
-                          " práticas e objetivas para cada tópico abordado."
-                          " Organize os flashcards da seguinte maneira:\n"
-                          "- Pergunta: A pergunta deve ser clara e direta, focando em conceitos importantes, "
-                          "definições,"
-                          "exemplos, ou detalhes específicos.\n"
-                          "- Resposta: A resposta deve ser precisa e objetiva, fornecendo a informação essencial para a"
-                          "pergunta.\n"
-                          "Aqui está o resumo/documento:\nObrigado!\n\n"
-                          "Exemplo de questão GPT teóricas e práticas\n\n"
-                          "Oi, Chat! Eu preciso criar questões práticas e teóricas para estudar o conteúdo do "
-                          "resumo/documento"
-                          " em anexo. Por favor, crie perguntas desafiadoras e variadas para cada tópico abordado."
-                          " Organize as questões da seguinte maneira:\n"
-                          "- Questões Teóricas: Perguntas que testem o entendimento dos conceitos, definições, "
-                          "e teorias.\n"
-                          "- Questões Práticas: Perguntas que envolvam a aplicação prática dos conceitos, resolução de "
-                          "problemas, e exemplos do mundo real.\n"
-                          "Aqui está o resumo/documento: \nObrigado!"
-              )
-calendar.add_event(event)
-print("Creating Exercises APT event...")
+print("Creating main event...")
 
 # spaced repetition reviews
 spaced_repetition_intervals = [1, 3, 7, 16, 35, 90]
